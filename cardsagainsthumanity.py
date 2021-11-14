36c6d5a930b0ff6c8803f9285e002b865196f785
Sami-Jagirdar <72996716+Sami-Jagirdar@users.noreply.github.com>
2021-11-14 15:12:19 +0300

    Add files via upload

--- cardsagainsthumanity.py
+++ cardsagainsthumanity.py
@@ -58,19 +58,53 @@ class Theme():
     
           
 
-
+import copy
 import pickle
-x = ['Family Friendly']
+theme_name_list = ['Family Friendly']
 theme_list =[]
-print("we are creating a new theme")
-theme_list.append(Theme(3))
-theme_list[-1].set_theme_name()
-x.append(t[-1].theme_name)
-theme_list[-1].set_prompt()
-theme_list[-1].set_answers()  
-custom_theme_file = open("newtheme.pickle", "wb") 
-pickle.dump(t[-1], custom_theme_file)
-custom_theme_file.close()
+read_theme_file = open("newtheme.pickle","rb")
+theme_list = pickle.load(read_theme_file)
+Default_theme  = Theme('Family Friendly', [
+                "Attention students! Principal Butthead is at home recovering from ___. We hope he'll be back soon."
+                ,"Coming soon! Batman vs. ___"
+                ,"This is gonna be the best sleepover ever. Once Mom goes to bed, it's time for ___!"
+                ,"My dad and I enjoy ___ together."
+                ,"Never fear, Captain ___ is here!"
+                ,"Kids, Dad is trying something new this week. It's called ___."
+                ,"CCN breaking news! Over half of Americans are now ___."
+                ,"Outback Steakhouse: No rules. Just ___."
+                ,"All I want for Christmas is ___."
+                ,"My favourite dinosaur is ___asaurus."
+                ],  [
+                "A cloud that rains diarrhea","Batman in a skirt","George Washington eating an apple","A big wet kiss from Great Aunt Sharon","Eight hours of video games.","Hanging out with Zendaya","Eating a lightbulb","A burrito smoothie","Covid 19","Eating Jelly"
+                ,"The police","Blowing up the moon","Turning into a super saiyan","Canada","Australia","United States","A hot cup of tea","Dirty upperware","Phineas and Ferb","Michael Jordan's Auntie"
+                ,"Cheeto fingers","TikTok","An iPhone with a broken screen","Polar Bears","Bob the builder dancing","Dora fighting Diego","A talking lion","A Democrat","A Republican","Idiots"
+                ,"The Magic School Bus","Ramen Noodles","Spaghetti Tacos","Pizza Hat","Climate Change","Box Fort","Chevy Truck","Honda Civic","Laptop","The loose skin at the joint of the elbow called a weenus"
+                ,"Pikachu","Politics","Crying in the bathroom","A bathtub of cottage cheese","JoJo Siwa","Ninjas","Happiness","Doing Karate","A lot of money","Being a good programmer"
+                ,"A hot air balloon powered by burps","The best game ever. Fortnite.","Darth Vader","Bacon strips","The government","Ice cream","The Harlem Shake","Youtube Rewind","Eating waffles","Ariana Grande"
+                ,"A dancing toddler","BBQ chips","C++","Python","IDK IDC ANYMORE","Random Prompt answer","Chicken nuggets","This is tedious","Read if you are not cool","1980's Music"
+                ],)
+
+theme_list.append(Default_theme)
+print(theme_list)
+def save_themes(theme_list):
+    print("we are creating a new theme")
+    theme_list.append(Theme(6))
+    theme_list[-1].set_theme_name()
+    theme_name_list.append(theme_list[-1].theme_name)
+    theme_list[-1].set_prompt()
+    theme_list[-1].set_answers()  
+    write_theme_file = open("newtheme.pickle", "wb") 
+    pickle.dump(theme_list, write_theme_file)
+    write_theme_file.close()
+
+
+'''Extra code to delete the themes/objects from the pickle file'''
+'''may be written.Such as clearing out the theme_list and then saving 
+it using pickle. Or converting the file contents into a list and then
+deleting themes from that list.
+Depends on time available. 
+'''
 
 
 
